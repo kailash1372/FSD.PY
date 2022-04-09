d65cccf66624745bc20ef734b5dbac31d1bb998c
KaiIash <86278203+KaiIash@users.noreply.github.com>
2022-04-09 12:56:30 +0530

    Update bfs.py

--- bfs.py
+++ bfs.py
@@ -10,15 +10,18 @@ Graph = {
 Discovered = []
 Queue = []
 
-def bfs_search(Graph,Discovered,Element):
+def bfs_search(Graph,Discovered,Element,ToFind,path):
      Queue.append(Element)
      Discovered.append(Element)
-     
+     path=path+Element+"--->"     
      while Queue:
          p = Queue.pop(0)
          print(p)
+         if p==ToFind:
+             return path
          for adj in Graph[p]:
             if adj not in Discovered:
+              path=path+adj+"--->"
               Discovered.append(adj)
               Queue.append(adj)
             
@@ -26,5 +29,6 @@ def bfs_search(Graph,Discovered,Element):
 
 
 
-
-bfs_search(Graph,Discovered,"A")     
+path=""
+bfs_search(Graph,Discovered,"A","D",path)     
+print(path)
